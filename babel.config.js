3d4bf200a2df167e9b8b5bd90290ada732a5c610
Error-331 <fsd4__123cxq__>
2019-06-29 04:56:15 +0300

    Replace babelrc with babel.config.js.

---
+++ babel.config.js
@@ -0,0 +1,39 @@
+module.exports = function (api) {
+    api.cache(true);
+
+    const presets = [
+        [
+            "@babel/env",
+            {
+                "modules": "auto",
+                "targets": {
+                    "browsers": [ ">0.25%", "not ie 11", "not op_mini all"]
+                },
+                "useBuiltIns": "usage",
+                "corejs": 3
+            }
+        ],
+        ["@babel/flow"],
+        ["@babel/react"]
+    ];
+
+    const plugins = [
+        [
+            "@babel/plugin-proposal-decorators",
+            {
+                "legacy": true
+            }
+        ],
+        [
+            "@babel/plugin-proposal-class-properties",
+            {
+                "loose": true
+            }
+        ]
+    ];
+
+    return {
+        presets,
+        plugins
+    };
+}
